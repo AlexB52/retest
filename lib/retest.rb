13382d01bbb2b890b2cb32ac10f4d56846e25103
Alexandre Barret <AlexB52@users.noreply.github.com>
2020-11-17 09:49:46 +1300

    Support for Ruby 2.4 (#23)
    * Update rescue block back begin syntaxt for compatibility with ruby 2.4
    
    * Extract retest to a build function
    
    * Remove Travis
    
    * Move test gems to Gemfile

--- lib/retest.rb
+++ lib/retest.rb
@@ -1,11 +1,12 @@
 require 'listen'
+require 'string/similarity'
+
 require "retest/version"
 require "retest/command"
 require "retest/repository"
 require "retest/test_options"
 require "retest/listen_options"
 require "retest/concerns/configurable"
-require 'string/similarity'
 
 module Retest
   include Configurable
@@ -13,12 +14,14 @@ module Retest
 
   def self.build(command:, clear_window: true)
     Listen.to('.', ListenOptions.to_h) do |modified, added, removed|
-      if modified.any?
-        system("clear") || system("cls") if clear_window
-        command.run(modified.first.strip)
+      begin
+        if modified.any?
+          system("clear") || system("cls") if clear_window
+          command.run(modified.first.strip)
+        end
+      rescue => e
+        puts "Something went wrong: #{e.message}"
       end
-    rescue => e
-      puts "Something went wrong: #{e.message}"
     end
   end
 end
