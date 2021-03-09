c663ab79353283e21f7f34fd8034b75f5d6586db
Alexandre Barret <alex@abletech.nz>
2021-03-10 08:25:38 +1300

    Add ruby-repository

---
+++ features/repositories/ruby-app/retest.rb
@@ -0,0 +1,36 @@
+class OutputFile
+  def initialize
+    create_file
+  end
+
+  def path
+    "output.log"
+  end
+  alias :to_s :path
+
+  def read
+    return unless File.exists?(path)
+
+    File.read(path)
+  end
+
+  def delete
+    return unless File.exists?(path)
+
+    File.delete(path)
+  end
+  alias :clear :delete
+
+  private
+
+  def create_file
+    File.open(path, "w")
+  end
+end
+
+$stdout.sync = true
+puts "starting test"
+@file = OutputFile.new
+@pid = Process.spawn "retest --ruby", out: @file.path
+puts "retest started in a thread"
+sleep
