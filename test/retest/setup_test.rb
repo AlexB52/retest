06380e920438c5b1828db724e7f072fa595777c8
Alexandre Barret <alex@abletech.nz>
2021-03-15 16:19:25 +1300

    Test retest gem is a rake setup

---
+++ test/retest/setup_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+module Retest
+  class SetupTest < MiniTest::Test
+    def setup
+      @subject = Setup.new
+    end
+
+    def test_repo_is_a_rake_setup
+      assert_equal :rake, @subject.type
+    end
+  end
+end
