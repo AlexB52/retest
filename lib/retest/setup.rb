2526eca92841739f00de9e5bfbe61c7c72086de9
Alexandre Barret <alex@abletech.nz>
2021-03-15 14:21:05 +1300

    Create Retest::Setup

---
+++ lib/retest/setup.rb
@@ -0,0 +1,39 @@
+module Retest
+  class Setup
+    def type
+      return :ruby unless has_gemfile?
+
+      if rspec?
+        :rspec
+      elsif rails?
+        :rails
+      elsif rake?
+        :rake
+      else
+        :ruby
+      end
+    end
+
+    private
+
+    def has_gemfile?
+      File.exist? 'Gemfile'
+    end
+
+    def rspec?
+      has_gem? 'rspec'
+    end
+
+    def rails?
+      File.exist? 'bin/rails'
+    end
+
+    def rake?
+      has_gem? 'rake'
+    end
+
+    def has_gem?(gem_name)
+      !`cat Gemfile | grep #{gem_name}`.empty?
+    end
+  end
+end
