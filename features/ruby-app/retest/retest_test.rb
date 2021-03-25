ffdac4f3053247b259c7c2eb558e5e490222c5b8
Alexandre Barret <alex@abletech.nz>
2021-03-25 15:05:05 +1300

    Test no files are being found on unmatched commands

--- features/ruby-app/retest/retest_test.rb
+++ features/ruby-app/retest/retest_test.rb
@@ -3,6 +3,7 @@ require 'minitest/autorun'
 require_relative 'retest_test/file_changes_test'
 require_relative 'retest_test/flags_test'
 require_relative 'retest_test/setup_test'
+require_relative 'retest_test/matching_unmatching_command_test'
 
 $stdout.sync = true
 
