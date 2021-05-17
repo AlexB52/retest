ad9b9391f75cf651054d275c44f33bf24c10d17c
Alexandre Barret <alex@abletech.nz>
2021-05-18 10:12:44 +1200

    Add interface tests for files ystem and its test fake

--- test/retest/file_system_test.rb
+++ test/retest/file_system_test.rb
@@ -1,13 +1,29 @@
 require 'test_helper'
 
+module FileSystemShared
+  def test_interface
+    assert_respond_to @subject, :exist?
+  end
+end
+
 module Retest
   class FileSystemTest < MiniTest::Test
+    include FileSystemShared
+
     def setup
       @subject = FileSystem
     end
 
     def test_exists?
       assert @subject.exist? 'test/retest/file_system_test.rb'
+    end
+  end
+
+  class FakeFSTest < MiniTest::Test
+    include FileSystemShared
+
+    def setup
+      @subject = FakeFS.new []
     end
   end
 end
