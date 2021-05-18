7f91be9728f5354d72ce5625ac9c2a6b2f850645
Alexandre Barret <alex@abletech.nz>
2021-05-18 15:47:12 +1200

    Add ruby-bare feature specs

---
+++ features/ruby-bare/program_test.rb
@@ -0,0 +1,7 @@
+require 'minitest/autorun'
+
+class SomeTest < MiniTest::Test
+  def test_the_truth
+    assert true
+  end
+end
---
+++ features/ruby-bare/retest/retest_test.rb
@@ -0,0 +1,110 @@
+require_relative 'test_helper'
+require 'minitest/autorun'
+
+$stdout.sync = true
+
+include FileHelper
+
+class SetupTest < Minitest::Test
+  def test_repository_setup
+    assert_equal :ruby, Retest::Setup.new.type
+  end
+end
+
+class AutoFlag < Minitest::Test
+  def test_start_retest
+    @output, @pid = launch_retest 'retest'
+
+    assert_match <<~OUTPUT, @output.read
+      Setup identified: [RUBY]. Using command: 'ruby <test>'
+      Launching Retest...
+      Ready to refactor! You can make file changes now
+    OUTPUT
+
+    modify_file('program.rb')
+
+    assert_match "Test File Selected: program_test.rb", @output.read
+  end
+end
+
+class FileChangesTest < Minitest::Test
+  def setup
+    @command = 'retest --ruby'
+  end
+
+  def teardown
+    end_retest @output, @pid
+  end
+
+  def test_start_retest
+    @output, @pid = launch_retest @command
+
+    assert_match <<~EXPECTED, @output.read
+      Launching Retest...
+      Ready to refactor! You can make file changes now
+    EXPECTED
+  end
+
+  def test_modifying_existing_file
+    @output, @pid = launch_retest @command
+
+    modify_file('program.rb')
+
+    assert_match "Test File Selected: program_test.rb", @output.read
+    assert_match "1 runs, 1 assertions, 0 failures, 0 errors, 0 skips", @output.read
+  end
+
+  def test_modifying_existing_test_file
+    @output, @pid = launch_retest @command
+
+    modify_file('program_test.rb')
+
+    assert_match "Test File Selected: program_test.rb", @output.read
+    assert_match "1 runs, 1 assertions, 0 failures, 0 errors, 0 skips", @output.read
+  end
+
+  def test_creating_a_new_test_file
+    @output, @pid = launch_retest @command
+
+    create_file 'foo_test.rb'
+
+    assert_match "Test File Selected: foo_test.rb", @output.read
+
+    delete_file 'foo_test.rb'
+  end
+
+  def test_creating_a_new_file
+    @output, @pid = launch_retest @command
+
+    create_file 'foo.rb'
+    assert_match <<~EXPECTED, @output.read
+      404 - Test File Not Found
+      Retest could not find a matching test file to run.
+    EXPECTED
+
+    create_file 'foo_test.rb'
+    assert_match "Test File Selected: foo_test.rb", @output.read
+
+    modify_file('program.rb')
+    assert_match "Test File Selected: program_test.rb", @output.read
+
+    modify_file('foo.rb')
+    assert_match "Test File Selected: foo_test.rb", @output.read
+
+    delete_file 'foo.rb'
+    delete_file 'foo_test.rb'
+  end
+
+  def test_untracked_file
+    create_file 'foo.rb', should_sleep: false
+    create_file 'foo_test.rb', should_sleep: false
+
+    @output, @pid = launch_retest @command
+
+    modify_file 'foo.rb'
+    assert_match "Test File Selected: foo_test.rb", @output.read
+
+    delete_file 'foo.rb'
+    delete_file 'foo_test.rb'
+  end
+end
