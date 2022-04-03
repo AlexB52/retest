caf59d486c2411626673e1191599c5749d968b32
Alexandre Barret <alexandre@hatchinvest.nz>
2022-04-03 22:22:25 +1200

    Fix warnings from ruby

--- test/retest/runners/runner_test.rb
+++ test/retest/runners/runner_test.rb
@@ -23,7 +23,7 @@ module Retest
       def test_run_all_tests
         runner = Runner.new("echo '<test>'")
 
-        out, _ = capture_subprocess_io { @subject.run_all_tests('file_path.rb file_path_two.rb') }
+        out, _ = capture_subprocess_io { runner.run_all_tests('file_path.rb file_path_two.rb') }
 
         assert_match "file_path.rb file_path_two.rb", out
       end
