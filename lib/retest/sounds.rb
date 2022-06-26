7141014c77f985bfbaaca293d4be07b0c4a81ce1
Alexandre Barret <alexandre@hatchinvest.nz>
2022-06-26 15:22:50 +1200

    Change start sound to Blow.aiff

--- lib/retest/sounds.rb
+++ lib/retest/sounds.rb
@@ -24,6 +24,8 @@ module Retest
           ['afplay', '/System/Library/Sounds/Sosumi.aiff']
         when :tests_pass
           ['afplay', '/System/Library/Sounds/Funk.aiff']
+        when :start
+          ['afplay', '/System/Library/Sounds/Blow.aiff']
         else
           raise ArgumentError.new("No sounds were found for type: #{sound}.")
         end
@@ -34,3 +36,19 @@ module Retest
     end
   end
 end
+
+# List of Mac Audio Files:
+# afplay /System/Library/Sounds/Basso.aiff
+# afplay /System/Library/Sounds/Bottle.aiff
+# afplay /System/Library/Sounds/Funk.aiff
+# afplay /System/Library/Sounds/Hero.aiff
+# afplay /System/Library/Sounds/Ping.aiff
+# afplay /System/Library/Sounds/Purr.aiff
+# afplay /System/Library/Sounds/Submarine.aiff
+# afplay /System/Library/Sounds/Blow.aiff
+# afplay /System/Library/Sounds/Frog.aiff
+# afplay /System/Library/Sounds/Glass.aiff
+# afplay /System/Library/Sounds/Morse.aiff
+# afplay /System/Library/Sounds/Pop.aiff
+# afplay /System/Library/Sounds/Sosumi.aiff
+# afplay /System/Library/Sounds/Tink.aiff
