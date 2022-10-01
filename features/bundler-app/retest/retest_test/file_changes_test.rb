84b99c5214c84aca0d30f855b46bc4971b6082a4
Alexandre Barret <barret.alx@gmail.com>
2022-10-01 15:20:57 +1300

    Add bundler-app to github actions

--- features/bundler-app/retest/retest_test/file_changes_test.rb
+++ features/bundler-app/retest/retest_test/file_changes_test.rb
@@ -1,6 +1,6 @@
 class FileChangesTest < Minitest::Test
   def setup
-    @command = 'retest --ruby'
+    @command = 'retest'
   end
 
   def teardown
@@ -19,63 +19,63 @@ class FileChangesTest < Minitest::Test
   def test_modifying_existing_file
     @output, @pid = launch_retest @command
 
-    modify_file('lib/bottles.rb')
+    modify_file('lib/bundler_app/bottles.rb')
 
-    assert_match "Test File Selected: test/test_bottles.rb", @output.read
+    assert_match "Test File Selected: test/bundler_app/test_bottles.rb", @output.read
     assert_match "12 runs, 12 assertions, 0 failures, 0 errors, 0 skips", @output.read
   end
 
   def test_modifying_existing_test_file
     @output, @pid = launch_retest @command
 
-    modify_file('test/test_bottles.rb')
+    modify_file('test/bundler_app/test_bottles.rb')
 
-    assert_match "Test File Selected: test/test_bottles.rb", @output.read
+    assert_match "Test File Selected: test/bundler_app/test_bottles.rb", @output.read
     assert_match "12 runs, 12 assertions, 0 failures, 0 errors, 0 skips", @output.read
   end
 
   def test_creating_a_new_test_file
     @output, @pid = launch_retest @command
 
-    create_file 'test_foo.rb'
+    create_file 'test/bundler_app/test_foo.rb'
 
-    assert_match "Test File Selected: test_foo.rb", @output.read
+    assert_match "Test File Selected: test/bundler_app/test_foo.rb", @output.read
 
-    delete_file 'test_foo.rb'
+    delete_file 'test/bundler_app/test_foo.rb'
   end
 
   def test_creating_a_new_file
     @output, @pid = launch_retest @command
 
-    create_file 'foo.rb'
+    create_file 'lib/bundler_app/foo.rb'
     assert_match <<~EXPECTED, @output.read
       404 - Test File Not Found
       Retest could not find a matching test file to run.
     EXPECTED
 
-    create_file 'test_foo.rb'
-    assert_match "Test File Selected: test_foo.rb", @output.read
+    create_file 'test/bundler_app/test_foo.rb'
+    assert_match "Test File Selected: test/bundler_app/test_foo.rb", @output.read
 
-    modify_file('lib/bottles.rb')
-    assert_match "Test File Selected: test/test_bottles.rb", @output.read
+    modify_file('lib/bundler_app/bottles.rb')
+    assert_match "Test File Selected: test/bundler_app/test_bottles.rb", @output.read
 
-    modify_file('foo.rb')
-    assert_match "Test File Selected: test_foo.rb", @output.read
+    modify_file('lib/bundler_app/foo.rb')
+    assert_match "Test File Selected: test/bundler_app/test_foo.rb", @output.read
 
-    delete_file 'foo.rb'
-    delete_file 'test_foo.rb'
+    delete_file 'lib/bundler_app/foo.rb'
+    delete_file 'test/bundler_app/test_foo.rb'
   end
 
   def test_untracked_file
-    create_file 'foo.rb', should_sleep: false
-    create_file 'test_foo.rb', should_sleep: false
+    create_file 'lib/bundler_app/foo.rb', should_sleep: false
+    create_file 'test/bundler_app/test_foo.rb', should_sleep: false
 
     @output, @pid = launch_retest @command
 
-    modify_file 'foo.rb'
-    assert_match "Test File Selected: test_foo.rb", @output.read
+    modify_file 'lib/bundler_app/foo.rb'
+    assert_match "Test File Selected: test/bundler_app/test_foo.rb", @output.read
 
-    delete_file 'foo.rb'
-    delete_file 'test_foo.rb'
+    delete_file 'lib/bundler_app/foo.rb'
+    delete_file 'test/bundler_app/test_foo.rb'
   end
 end
