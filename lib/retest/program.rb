6b7677387d4457cd0557fbda63f0733c6cc22108
Robert Starsi <klevo@klevo.sk>
2024-08-03 15:27:57 +0200

    refactored into Program

--- lib/retest/program.rb
+++ lib/retest/program.rb
@@ -1,10 +1,13 @@
 module Retest
   class Program
-    attr_accessor :runner, :repository, :command
+    attr_accessor :runner, :repository, :command, :all_runner
     def initialize(runner: nil, repository: nil, command: nil)
       @runner = runner
       @repository = repository
       @command = command
+      @all_runner = Retest::Runners.runner_for(
+        Retest::Command.for_options(Retest::Options.new(["--all"]), quiet: true).to_s
+      )
     end
 
     def run(modified, added, removed)
@@ -13,6 +16,12 @@ module Retest
       system('clear 2>/dev/null') || system('cls 2>/dev/null')
 
       runner.run (modified + added).first, repository: repository
+    end
+
+    def run_all
+      system('clear 2>/dev/null') || system('cls 2>/dev/null')
+
+      all_runner.run
     end
 
     def diff(branch)
