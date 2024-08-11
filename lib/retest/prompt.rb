6e366130799126c5a197076968ece5243ba42307
Alexandre Barret <barret.alx@gmail.com>
2024-08-11 20:43:36 +1200

    Test question_ask flag

--- lib/retest/prompt.rb
+++ lib/retest/prompt.rb
@@ -6,24 +6,30 @@ module Retest
     def initialize(input: nil, output: nil)
       @input  = input || $stdin
       @output = output || $stdout
+      @question_asked = false
+    end
+
+    def question_asked?
+      @question_asked
     end
 
     def ask_which_test_to_use(path, files)
-      changed
-      notify_observers(:question)
-      options = options(files)
+      ask_question do
+        changed
+        notify_observers(:question)
+        options = options(files)
 
-      output.puts(<<~QUESTION)
-        We found few tests matching: #{path}
+        output.puts(<<~QUESTION)
+          We found few tests matching: #{path}
 
-        #{list_options(options.keys)}
+          #{list_options(options.keys)}
 
-        Which file do you want to use?
-        Enter the file number now:
-      QUESTION
-      output.print("> ")
-
-      options.values[input.gets.chomp.to_i]
+          Which file do you want to use?
+          Enter the file number now:
+        QUESTION
+        output.print("> ")
+        options.values[input.gets.chomp.to_i]
+      end
     end
 
     def puts(*args)
@@ -32,6 +38,14 @@ module Retest
 
     def read_output
       output.tap(&:rewind).read
+    end
+
+    def ask_question
+      old_question_asked = @question_asked
+      @question_asked = true
+      yield
+    ensure
+      @question_asked = old_question_asked
     end
 
     private
--- test/retest/prompt_test.rb
+++ test/retest/prompt_test.rb
@@ -49,6 +49,44 @@ module Retest
       EXPECTED
     end
 
+    class ListeningIO
+      attr_accessor :io
+      def initialize
+        @io = StringIO.new
+      end
+
+      def gets
+        loop do
+          sleep 0.0001
+          line = @io.gets.to_s
+          break line unless line.empty?
+        end
+      end
+    end
+
+    def test_question_asked_when_asking_question
+      files = %w(
+        test/models/taxation/holdings_test.rb
+        test/models/schedule/holdings_test.rb
+        test/models/holdings_test.rb
+        test/models/performance/holdings_test.rb
+        test/lib/csv_report/holdings_test.rb
+      )
+
+      @subject.input = stdin = ListeningIO.new
+
+      th = Thread.new do
+        @subject.ask_which_test_to_use("app/models/valuation/holdings.rb", files)
+      end
+      sleep 0.0001
+      assert @subject.question_asked?
+      stdin.io = StringIO.new("1\n")
+      th.join
+
+      refute @subject.question_asked?
+      assert_equal "test/models/schedule/holdings_test.rb", th.value
+    end
+
     def test_read_output
       @subject.output.puts "hello world\n"
 
@@ -73,6 +111,18 @@ module Retest
       @subject.ask_which_test_to_use("app/models/valuation/holdings.rb", files)
 
       assert_includes observer.notepad, MethodCall.new(name: :update, args: [:question])
+    end
+
+    def test_ask_question
+      refute @subject.question_asked?
+      @subject.ask_question do
+        assert @subject.question_asked?
+      end
+      refute @subject.question_asked?
+    end
+
+    def test_question_flag_when_asking_for_file
+
     end
   end
 end
