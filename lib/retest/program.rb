19231f47b537e0c7951d8babb329eae42bcbbe9b
Alexandre Barret <AlexB52@users.noreply.github.com>
2024-08-13 21:53:23 +1200

    listen to user inputs in executable (#213)
    * Listen to user inputs in executable
    
          * 'h' or 'help'    -> Prints help
          * 'p' or 'pause'   -> Pauses Retest. Tests aren't run until unpaused.
          * 'u' or 'unpause' -> Unpauses Retest
          * <Enter>          -> Runs last changed triggered command
          * 'ra', 'run all'  -> Runs all tests
          * 'e' or 'exit'    -> Exits Retest

--- lib/retest/program.rb
+++ lib/retest/program.rb
@@ -1,18 +1,27 @@
+require_relative 'program/pausable'
+
 module Retest
   class Program
+    include Pausable
+
     attr_accessor :runner, :repository, :command
-    def initialize(runner: nil, repository: nil, command: nil)
+    def initialize(runner: nil, repository: nil, command: nil, clear_window: true)
       @runner = runner
       @repository = repository
       @command = command
+      @clear_window = clear_window
+      initialize_pause(false)
     end
 
     def run(modified, added, removed)
       repository.sync(added: added, removed: removed)
       runner.sync(added: added, removed: removed)
-      system('clear 2>/dev/null') || system('cls 2>/dev/null')
 
+      return if paused?
+
+      clear_terminal
       runner.run (modified + added).first, repository: repository
+      yield if block_given?
     end
 
     def diff(branch)
@@ -24,6 +33,14 @@ module Retest
 
       puts "Running tests..."
       runner.run_all_tests command.format_batch(*test_files)
+    end
+
+    private
+
+    def clear_terminal
+      return unless @clear_window
+
+      system('clear 2>/dev/null') || system('cls 2>/dev/null')
     end
   end
 end
--- test/retest/program_test.rb
+++ test/retest/program_test.rb
@@ -3,8 +3,26 @@ require 'test_helper'
 # TODO : write missing tests
 module Retest
   class ProgramTest < MiniTest::Test
-    def setup
-      @subject = Program.new
+    class PauseTest < Minitest::Test
+      def setup
+        @subject = Program.new(repository: Repository.new, clear_window: false)
+      end
+
+      def test_paused?
+        refute @subject.paused?
+
+        @subject.pause
+        assert @subject.paused?
+
+        @subject.resume
+        refute @subject.paused?
+      end
+
+      def test_no_run_when_paused
+        @subject.runner = RaisingRunner.new
+        @subject.pause
+        @subject.run(['modified'], ['added'], ['removed'])
+      end
     end
   end
 end
