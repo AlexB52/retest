3a698523c683454fe475e0a491ce42001818b72d
Alexandre Barret <barret.alx@gmail.com>
2024-12-04 08:19:42 +1300

    Create Watchexec feature specs

---
+++ features/watchexec-ruby-app/retest/retest_test.rb
@@ -0,0 +1,16 @@
+require 'retest'
+require 'byebug'
+require_relative 'support/test_helper'
+require 'minitest/autorun'
+require_relative 'retest_test/file_changes_test'
+require_relative 'retest_test/setup_test'
+require_relative 'retest_test/matching_unmatching_command_test'
+
+$stdout.sync = true
+
+include FileHelper
+include OutputHelper
+
+module WatchexecRuby
+  COMMAND = 'retest'
+end
---
+++ features/watchexec-ruby-app/retest/retest_test/file_changes_test.rb
@@ -0,0 +1,87 @@
+module WatchexecRuby
+  class FileChangesTest < Minitest::Test
+    def teardown
+      end_retest
+    end
+
+    def test_start_retest
+      launch_retest(COMMAND)
+
+      assert_match <<~EXPECTED, read_output
+        Setup identified: [RUBY]. Using command: 'bundle exec ruby <test>'
+        Watcher: [WATCHEXEC]
+        Launching Retest...
+        Ready to refactor! You can make file changes now
+      EXPECTED
+    end
+
+    def test_modifying_existing_file
+      launch_retest(COMMAND)
+
+      modify_file('lib/bottles.rb')
+
+      read_output do |output|
+        assert_match "Test file: test/bottles_test.rb", output
+        assert_match "12 runs, 12 assertions, 0 failures, 0 errors, 0 skips", output
+      end
+    end
+
+    def test_modifying_existing_test_file
+      launch_retest(COMMAND)
+
+      modify_file('test/bottles_test.rb')
+
+      read_output do |output|
+        assert_match "Test file: test/bottles_test.rb", output
+        assert_match "12 runs, 12 assertions, 0 failures, 0 errors, 0 skips", output
+      end
+    end
+
+    def test_creating_a_new_test_file
+      launch_retest(COMMAND)
+
+      create_file 'foo_test.rb'
+
+      assert_match "Test file: foo_test.rb", read_output
+
+    ensure
+      delete_file 'foo_test.rb'
+    end
+
+    def test_creating_a_new_file
+      launch_retest(COMMAND)
+
+      create_file 'foo.rb'
+      assert_match <<~EXPECTED, read_output
+        FileNotFound - Retest could not find a matching test file to run.
+      EXPECTED
+
+      create_file 'foo_test.rb'
+      assert_match "Test file: foo_test.rb", read_output
+
+      modify_file('lib/bottles.rb')
+      assert_match "Test file: test/bottles_test.rb", read_output
+
+      modify_file('foo.rb')
+      assert_match "Test file: foo_test.rb", read_output
+
+    ensure
+      delete_file 'foo.rb'
+      delete_file 'foo_test.rb'
+    end
+
+    def test_untracked_file
+      create_file 'foo.rb', should_sleep: false
+      create_file 'foo_test.rb', should_sleep: false
+
+      launch_retest(COMMAND)
+
+      modify_file 'foo.rb'
+      assert_match "Test file: foo_test.rb", read_output
+
+    ensure
+      delete_file 'foo.rb'
+      delete_file 'foo_test.rb'
+    end
+  end
+end
