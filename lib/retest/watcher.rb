2deadc82be976da46d67d32baec9db03ebbfed2b
Alexandre Barret <AlexB52@users.noreply.github.com>
2024-12-05 18:40:07 +1300

    V2/watchexec feature test (#232)
    * Create Watchexec feature specs
    * Build watchexec in separate image
    * Add command to build watchexec image

--- lib/retest/watcher.rb
+++ lib/retest/watcher.rb
@@ -41,48 +41,81 @@ module Retest
       end
 
       def self.watch(dir:, extensions:, polling: false)
-        require 'open3'
         command = "watchexec --exts #{extensions.join(',')} -w #{dir} --emit-events-to stdio --no-meta --only-emit-events"
+        files = VersionControl.files(extensions: extensions).zip([]).to_h
 
-        # watch_rd, watch_wr = IO.pipe
-        # pid = Process.spawn(command, out: watch_wr)
-        # at_exit do
-        #   Process.kill("TERM", pid) if pid
-        #   watch_rd.close
-        #   watch_wr.close
-        # end
+        watch_rd, watch_wr = IO.pipe
+        pid = Process.spawn(command, out: watch_wr)
+        at_exit do
+          Process.kill("TERM", pid) if pid
+          watch_rd.close
+          watch_wr.close
+        end
 
         Thread.new do
-          files = VersionControl.files(extensions: extensions).zip([]).to_h
+          loop do
+            ready = IO.select([watch_rd])
+            readable_connections = ready[0]
+            readable_connections.each do |conn|
+              data = conn.readpartial(4096)
+              change = /^(?:create|remove|rename|modify):(?<path>.*)/.match(data.strip)
 
-          Open3.popen3(command) do |stdin, stdout, stderr, wait_thr|
-            loop do
-              ready = IO.select([stdout])
-              readable_connections = ready[0]
-              readable_connections.each do |conn|
-                data = conn.readpartial(4096)
-                change = /^(?:create|remove|rename|modify):(?<path>.*)/.match(data.strip)
-                if change
-                  modified, added, removed = result = [[], [], []]
-                  path = Pathname(change[:path]).relative_path_from(Dir.pwd).to_s
-                  file_exist = File.exist?(path)
-                  file_cached = files.key?(path)
-                  if file_exist && file_cached
-                    modified << path
-                  elsif file_exist && !file_cached
-                    added << path
-                    files[path] = nil
-                  elsif !file_exist && file_cached
-                    removed << path
-                    files.delete(path)
-                  end
+              next unless change
 
-                  yield result
-                end
+              path = Pathname(change[:path]).relative_path_from(Dir.pwd).to_s
+              file_exist = File.exist?(path)
+              file_cached = files.key?(path)
+
+              modified, added, removed = result = [[], [], []]
+              if file_exist && file_cached
+                modified << path
+              elsif file_exist && !file_cached
+                added << path
+                files[path] = nil
+              elsif !file_exist && file_cached
+                removed << path
+                files.delete(path)
               end
+
+              yield result
             end
           end
         end
+
+        # require 'open3'
+        # Thread.new do
+        #   files = VersionControl.files(extensions: extensions).zip([]).to_h
+
+        #   Open3.popen3(command) do |stdin, stdout, stderr, wait_thr|
+        #     loop do
+        #       ready = IO.select([stdout])
+        #       readable_connections = ready[0]
+        #       readable_connections.each do |conn|
+        #         data = conn.readpartial(4096)
+        #         change = /^(?:create|remove|rename|modify):(?<path>.*)/.match(data.strip)
+
+        #         next unless change
+
+        #         path = Pathname(change[:path]).relative_path_from(Dir.pwd).to_s
+        #         file_exist = File.exist?(path)
+        #         file_cached = files.key?(path)
+
+        #         modified, added, removed = result = [[], [], []]
+        #         if file_exist && file_cached
+        #           modified << path
+        #         elsif file_exist && !file_cached
+        #           added << path
+        #           files[path] = nil
+        #         elsif !file_exist && file_cached
+        #           removed << path
+        #           files.delete(path)
+        #         end
+
+        #         yield result
+        #       end
+        #     end
+        #   end
+        # end
       end
     end
   end
