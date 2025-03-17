7c05263cc7b52edcbac4d5291df0585126dd555c
Alexandre Barret <AlexB52@users.noreply.github.com>
2025-03-18 09:09:39 +1300

    Refactor test fixtures (#253)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,43 +3,7 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "retest"
 require "byebug"
 require "minitest/autorun"
-
-WAITING_TIME = 0.0001
-
-def wait(time = WAITING_TIME)
-  sleep time
-end
-
-FakeFS = Struct.new(:files) do
-  def exist?(value)
-    files.include? value
-  end
-end
-
-class RaisingRunner
-  class MethodCallError < StandardError; end
-
-  def initialize
-  end
-
-  def sync(added:, removed:)
-  end
-
-  def run(file, repository:)
-    raise MethodCallError, "#{__method__} should not be called"
-  end
-end
-
-def wait_until(max_attempts: 10)
-  attempts = 0
-  begin
-    yield
-  rescue Minitest::Assertion => e
-    raise e if attempts >= max_attempts
-    wait
-    attempts += 1
-  end
-end
+require "support/fixtures"
 
 module Retest
   # Remove Watchexec when not installed
