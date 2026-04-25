5af500f2852bca698d9c1b9e55ddce623fa34bc2
Alexandre Barret <barret.alx@gmail.com>
2026-04-25 13:39:27 +1200

    Update expected help in e2e tests

--- features/ruby-bare/retest/flags/help.rb
+++ features/ruby-bare/retest/flags/help.rb
@@ -9,9 +9,9 @@ class TestHelpFlag < Minitest::Test
     launch_retest 'retest --help'
 
     assert_output_matches <<~EXPECTED
-      Usage: retest  [OPTIONS] [COMMAND]
+      Usage: retest [options] [COMMAND]
 
-      Watch a file change and run it matching spec.
+      Watch files and rerun matching tests when they change.
     EXPECTED
   end
 
@@ -19,9 +19,9 @@ class TestHelpFlag < Minitest::Test
     launch_retest 'retest -h'
 
     assert_output_matches <<~EXPECTED
-      Usage: retest  [OPTIONS] [COMMAND]
+      Usage: retest [options] [COMMAND]
 
-      Watch a file change and run it matching spec.
+      Watch files and rerun matching tests when they change.
     EXPECTED
   end
 end
